57d6ed8a7ccf6d3bca6b3fee6373f4086eae69f8
Prince Asiedu <princekofasiedu@gmail.com>
2022-10-11 14:24:49 +0000

    Added contruction message to website

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ let img = document.querySelector('#main_header_img')
 window.addEventListener('scroll', function() {
     let value = window.scrollY
     catchText.style.top = value * 0.5 + 'px';
-    img.style.top = value * 0.05 + 'px';
+    img.style.left = value * 0.05 + 'px';
 })
